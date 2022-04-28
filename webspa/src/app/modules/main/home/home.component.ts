0ae9d4e81b027ec4c81a616f2dd4e8f18e17c7d3
rodrigocrespin <62776976+rodrigocrespin@users.noreply.github.com>
2022-04-28 22:11:50 +0000

    Deploying to gh-pages from  @ 482c96272a1d5cdb39a737c5a18efe48c8c0f72c 🚀

--- webspa/src/app/modules/main/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  templateUrl: './home.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class HomeComponent {
-  title = 'video-call-client';
-}
--- webspa/src/app/modules/main/shell/app-container/app-container.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: 'app-container.component.html',
-  styleUrls: ['./app-container.component.scss']
-})
-export class AppContainerComponent {
-}
-
